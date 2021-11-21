b51fbc2f587dda526d58694f809b7a2e0ec40c36
Fernanda Cherini <fer.azvdo@gmail.com>
2021-11-21 16:25:55 -0300

    Finish home section

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import "./index.scss";
+import LocomotiveScroll from "locomotive-scroll";
+
+const scroll = new LocomotiveScroll({
+  el: document.querySelector("[data-scroll-container]"),
+  smooth: true,
+});
